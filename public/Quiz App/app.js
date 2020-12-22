dc46851a1bb4efe6ae4e33cc1e3203dac0572b84
Mustafa-ilahi <mustafailahi586@gmail.com>
2020-12-23 02:43:00 +0500

    updated

---
+++ public/Quiz App/app.js
@@ -0,0 +1,99 @@
+function question1(){
+    var q1_correctAns = document.getElementById("q1-correctAns");
+    if(q1_correctAns.checked == true){
+        alert("Correct ans!");
+    }
+    else{
+        alert("Wrong ans")
+    }
+}
+
+function question2(){
+    var q2_correctAns = document.getElementById("q2-correctAns");
+    if(q2_correctAns.checked == true){
+        alert("Correct ans!");
+    }
+    else{
+        alert("Wrong ans")
+    }
+}
+
+function question3(){
+    var q3_correctAns = document.getElementById("q3-correctAns");
+    if(q3_correctAns.checked == true){
+        alert("Correct ans!");
+    }
+    else{
+        alert("Wrong ans")
+    }
+}
+
+function question4(){
+    var q4_correctAns = document.getElementById("q4-correctAns");
+    if(q4_correctAns.checked == true){
+        alert("Correct ans!");
+    }
+    else{
+        alert("Wrong ans")
+    }
+}
+
+function question5(){
+    var q5_correctAns = document.getElementById("q5-correctAns");
+    if(q5_correctAns.checked == true){
+        alert("Correct ans!");
+    }
+    else{
+        alert("Wrong ans")
+    } 
+}
+
+function question6(){
+    var q6_correctAns = document.getElementById("q6-correctAns");
+    if(q6_correctAns.checked == true){
+        alert("Correct ans!");
+    }
+    else{
+        alert("Wrong ans")
+    } 
+}
+
+function question7(){
+    var q7_correctAns = document.getElementById("q7-correctAns");
+    if(q7_correctAns.checked == true){
+        alert("Correct ans!");
+    }
+    else{
+        alert("Wrong ans")
+    }
+}
+
+function question8(){
+    var q8_correctAns = document.getElementById("q8-correctAns");
+    if(q8_correctAns.checked == true){
+        alert("Correct ans!");
+    }
+    else{
+        alert("Wrong ans")
+    }
+}
+
+function question9(){
+    var q9_correctAns = document.getElementById("q9-correctAns");
+    if(q9_correctAns.checked == true){
+        alert("Correct ans!");
+    }
+    else{
+        alert("Wrong ans")
+    }
+}
+
+function question10(){
+    var q10_correctAns = document.getElementById("q10-correctAns");
+    if(q10_correctAns.checked == true){
+        alert("Correct ans!");
+    }
+    else{
+        alert("Wrong ans")
+    }
+}
