c9ea2eff80d1bbefe8a3335ee83a8336229079ed
Mustafa-ilahi <mustafailahi586@gmail.com>
2020-12-28 23:46:08 +0500

    firebase authentication

--- public/Chapter 1 to 67 (JS)/Firebase authentication/app.js
+++ public/Chapter 1 to 67 (JS)/Firebase authentication/app.js
@@ -1,6 +1,35 @@
 let signUp = () => {
     let email = document.getElementById("email");
     let password = document.getElementById("password");
-    console.log(email.value);
-    console.log(password.value);
+    // console.log(email.value);
+    // console.log(password.value);
+
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+  .then((user) => {
+    console.log("successfully SignUp");
+    console.log(user);
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(" " + errorMessage);
+  });
+
 }
+let logIn = () => {
+    let email = document.getElementById("login-email");
+    let password = document.getElementById("login-password");
+    // console.log(email.value , password.value);
+    firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+    .then((user) => {
+        console.log("successfully login");
+        console.log(user)
+    })
+    .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    alert(" " + errorMessage);
+    });
+
+}
+ 
