4a4a2d0d58ce902cdc59ec929e5522f28ffc8e52
Mustafa-ilahi <mustafailahi586@gmail.com>
2020-12-31 00:23:06 +0500

    some updates

--- public/Chat App/app.js
+++ public/Chat App/app.js
@@ -1,8 +1,65 @@
-// console.log("Hello");
+// console.log(firebase);
 let signUp = () => {
-    alert("chlgya")
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    // console.log(email.value,password.value);
+    let firstName = document.getElementById("firstName");
+    let lastName = document.getElementById("lastName");
+    // console.log(firstName.value, lastName.value);
+    let userName = {
+        First_Name : firstName.value,
+        Last_Name : lastName.value
+    }
+    let key = firebase.database().ref('users/key').push().key;
+    console.log(key);
+    firebase.database().ref('users/').child(key).set(userName);
+
+//     firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+//   .then((user) => {
+//       console.log("Successfully Sign Up");
+//       console.log(user);
+//       swal({
+//         title: "Successfully Sign Up!",
+//         icon: "success",
+//       });
+
+//   })
+//   .catch((error) => {
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     console.log(errorMessage);
+//     swal({
+//         title: errorMessage,
+//         icon: "error",
+//       });
+//   });
+
 }
 
 let login = () => {
-    alert("ok hai bhai")
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+    .then((user) => {
+        console.log("Successfully Login");
+        console.log(user);
+        swal({
+            title: "Successfully Login!",
+            icon: "success",
+          });
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      swal({
+        title: errorMessage,
+        icon: "error",
+      });
+    });
 }
+
+// swal({
+//     title: "Successfully Sign Up!",
+//     icon: "success",
+//   });
